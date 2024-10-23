b28e0556fb68c4033f0f1b2f820802de64b7ba06
Achintha <72160964+dev-achintha@users.noreply.github.com>
2024-10-23 18:26:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
 
-require_once 'config/bootstrap.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
+$uri = explode('/', $uri);
 
-if ((isset($uri[1]) && $uri[1] != 'restful') || !isset($uri[2])) {
+$restfulIndex = array_search('restful', $uri);
+if ($restfulIndex === false || !isset($uri[$restfulIndex + 1])) {
     $response = [
         'status' => 'error',
         'message' => 'Invalid or empty query.',
         'data' => [],
     ];
-    
     header("HTTP/1.1 404 Not Found");
     header('Content-Type: application/json');
     echo json_encode($response);
     exit();
 }
 
-require_once PROJECT_ROOT_PATH."/controller/HorizonStudents.php";
+require_once "controller/HorizonStudents.php";
+
 $feedController = new HorizonStudents();
 $feedController->performAction();
 ?>
